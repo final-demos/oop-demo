3d3ae6cad04b98a1d9f9a69b0513f4d45c6307bc
ibmpariwesh <ibmpariwesh@gmail.com>
2021-10-29 12:49:47 +0530

    first commit

---
+++ src/com/oop/demo/BMSApplication.java
@@ -0,0 +1,13 @@
+package com.oop.demo;
+
+public class BMSApplication {
+	public static void main(String[] args) {
+		Book book;// reference or variable
+		Book book1 = new Book("Ram");// reference + object
+		System.out.println(book1.author);
+		new Object();// only object is created
+
+//		book1.author = "Ram"; //bad code
+		
+	}
+}
